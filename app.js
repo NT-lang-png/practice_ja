bd0a8602ec5e7becfd363d695a3a6aa8420f747e
EC2 Default User <ec2-user@ip-172-31-41-77.ap-northeast-1.compute.internal>
2024-11-05 12:12:04 +0000

    [start]javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+let hello = "Hello World";
+alert(hello);
+
+let int1 = "1";
+
+let int2 = -10;
+
+let float = 3.14;
+
+let str1 = 'javascriptを覚えよう';
+
+alert(4 + 3);
+
+alert(8 - 5);
+
+alert(2 * 6);
+
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World';
